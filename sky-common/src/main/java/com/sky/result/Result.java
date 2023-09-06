64a78e73444dbfff6fb4c3c5e7910fe1cb0455c0
lei <1692704089@qq.com>
2023-09-06 23:04:37 +0800

    员工分页查询，扩展消息转换器，日期统一转换

--- sky-common/src/main/java/com/sky/result/Result.java
+++ sky-common/src/main/java/com/sky/result/Result.java
@@ -29,6 +29,12 @@ public class Result<T> implements Serializable {
         result.msg = msg;
         return result;
     }
+    public static <T> Result<T> success(T object) {
+        Result<T> result = new Result<T>();
+        result.data = object;
+        result.code = 1;
+        return result;
+    }
 
 //     登录失败
     public static <T> Result<T> error(String msg) {
--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -4,8 +4,10 @@ import com.sky.constant.JwtClaimsConstant;
 import com.sky.constant.MessageConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.service.impl.EmployeeServiceImpl;
@@ -15,10 +17,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -86,6 +85,19 @@ public class EmployeeController {
         employeeService.save(employeeDTO);
 
         return Result.success(employeeDTO, MessageConstant.EMPLOYEE_SAVE_SUCCESS);
+    }
+
+    /***
+     * 分页查询员工
+     * @param employeePageQueryDTO
+     * @return
+     */
+    @ApiOperation("分页查询员工")
+    @GetMapping("/page")
+    public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
+        log.info("分页查询员工：{}", employeePageQueryDTO);
+        PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(pageResult);
     }
 
 }
