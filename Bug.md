dd45269ab5d5bc950864d57bbcab2ec345ad216f
lei <1692704089@qq.com>
2023-09-07 09:57:22 +0800

    修改员工信息

---
+++ Bug.md
@@ -0,0 +1,13 @@
+# 记录bug
+## update失败，提示语法错误
+![](https://raw.githubusercontent.com/lei1692/typora/main/image/202309070951046.png)
+![](https://raw.githubusercontent.com/lei1692/typora/main/image/202309070952826.png)
+驼峰命名指的是关系映射，SQL语句的编写还是要按照数据库的命名规范，即下划线命名法，所以在编写SQL语句时，要注意表名和字段名的大小写，以及表名和字段名之间的下划线。
+![](https://raw.githubusercontent.com/lei1692/typora/main/image/202309070953970.png)
+修改为和数据库一致
+![](https://raw.githubusercontent.com/lei1692/typora/main/image/202309070956640.png)
+
+
+
+
+
--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -113,4 +113,20 @@ public class EmployeeController {
         return Result.success();
     }
 
+    @ApiOperation("根据id查询信息")
+    @GetMapping("/{id}")
+    public Result<Employee> getById(@PathVariable("id") Long id){
+        log.info("根据id查询信息：{}", id);
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    @ApiOperation("根据id修改员工信息")
+    @PutMapping
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("根据id修改员工信息：{}", employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
+
 }
