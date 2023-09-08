d5355193d49619866196f12476fa29acb824ec62
lei <1692704089@qq.com>
2023-09-08 14:00:02 +0800

    阿里云图像存储

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,39 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "公共上传接口")
+@Slf4j
+public class CommonController {
+    @Autowired
+    private AliOssUtil aliOssUtil;
+    @ApiOperation("上传文件")
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file){
+        log.info("上传文件：{}", file.getOriginalFilename());
+
+        String extension = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+        String ObjectName = System.currentTimeMillis() + extension;
+        try {
+            String filePath = aliOssUtil.upload(file.getBytes(), ObjectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+
+        return null;
+    }
+}
